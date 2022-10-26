93af5bbd7bd1e1bcca79b3092d5fb80ee5c52537
FianKisero <kiserofian11@gmail.com>
2022-10-26 09:14:25 -0700

    reverse

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -2,7 +2,7 @@
 #include <stdio.h>
 
 /**
- * print_array - prints the elements of an array of integers, followed by a new line.
+ * print_array - prints n elements of an array
  * @a: array of integers
  * @n: number of items to print
  * Return: void
