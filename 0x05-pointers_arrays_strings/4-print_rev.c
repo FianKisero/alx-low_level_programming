da61043b803fcb3120e12222a89d650755253960
FianKisero <kiserofian11@gmail.com>
2022-10-26 07:28:55 -0700

    reverse

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -6,25 +6,18 @@
 
 void print_rev(char *s)
 {
-	int len = 0, i = 0;
-	len = _slen(s);
+	int i = 0;
 
-	for (i = len - 1; i >= 0; i--)
+	while (s[i] != '\0')
+	{
+		i++;
+	}
+	i--;
+	while (i >= 0)
+	{
 		_putchar(s[i]);
+		i--;
+	}
 
 	_putchar('\n');
-}
-/**
- * _slen - returns the length of a string
- * @s: string
- * Return: returns lenght;
- */
-int _slen(char *s)
-{
-	int count, i;
-	i = 0;
-	for (count = 0; s[count] != '\0'; count++)
-		i++;
-
-	return (i);
 }
