f7e54be47a23ceafa033a97839a40c09b5003d4a
FianKisero <kiserofian11@gmaile.com>
2023-03-28 19:49:52 +0300

    makefiles

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -1,30 +1,31 @@
 #!/usr/bin/python3
-"""Defines an island perimeter measuring function."""
+"""Module that calculates the perimeter of an island in a grid."""
+
+
+def num_water_neighbors(grid, i, j):
+    """Returns the number of water neighbors a cell has in a grid."""
+
+    num = 0
+
+    if i <= 0 or not grid[i - 1][j]:
+        num += 1
+    if j <= 0 or not grid[i][j - 1]:
+        num += 1
+    if j >= len(grid[i]) - 1 or not grid[i][j + 1]:
+        num += 1
+    if i >= len(grid) - 1 or not grid[i + 1][j]:
+        num += 1
+
+    return num
 
 
 def island_perimeter(grid):
-    """Return the perimiter of an island.
-    The grid represents water by 0 and land by 1.
-    Args:
-        grid (list): A list of list of integers representing an island.
-    Returns:
-        The perimeter of the island defined in grid.
-    """
-    # 1. We first find the width and height of the grid.
-    # 2. We then iterate through the grid and find the number of edges and the
-    # number of nodes.
-    # 3. We then return the number of nodes * 4 - number of edges * 2.
-    # Time Complexity: O(n^2)
-    width = len(grid[0])
-    height = len(grid)
-    edges = 0
-    size = 0
+    """Returns the perimeter of the island in grid."""
 
-    for i in range(height):
-        for j in range(width):
-            if grid[i][j] == 1:
-                size += 1
-                if (j > 0 and grid[i][j - 1] == 1):
-                    edges += 1
-                if (i > 0 and grid[i - 1][j] == 1):
-                    edges += 1
+    perimeter = 0
+    for i in range(len(grid)):
+        for j in range(len(grid[i])):
+            if grid[i][j]:
+                perimeter += num_water_neighbors(grid, i, j)
+
+    return perimeter
